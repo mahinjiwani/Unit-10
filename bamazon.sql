6de345684a410f5e8ad03357e2cbbc6baeaaa15b
Mahin Jiwani <mahinjiwani@Mahins-MacBook-Air.local>
2019-05-19 02:40:45 -0400

    push

---
+++ bamazon.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE amazonDB;
+
+USE amazonDB;
+
+CREATE TABLE products (
+	item_id VARCHAR(30),
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price INTEGER(10),
+    stock_quantity INTEGER(10)
+);
+
+INSERT INTO products (item_id, product_name, department name, price, stock_quantity)
+VALUES (
+
+    
+    
